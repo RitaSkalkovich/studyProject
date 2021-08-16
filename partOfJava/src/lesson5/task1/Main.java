a554404d156e8f2cb5f9730f9d8bb03be0b672c7
RitaSkalkovich <skalkovichrita140295@gmail.com>
2021-08-16 19:04:00 +0200

    Create objects and set weight and kind values

---
+++ partOfJava/src/lesson5/task1/Main.java
@@ -0,0 +1,42 @@
+package lesson5.task1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Bird eagle = new Bird("Ястреб", 3.6);
+        Bird sparrow = new Bird("Воробей", 1);
+        Bird penguin = new Bird("Пингвин", 11);
+
+
+    }
+}
+//
+//
+//
+//        double allWeight = eagle.weight + sparrow.weight + penguin.weight;
+//        System.out.println(allWeight);
+
+
+//        System.out.println(eagle.getKind());
+//        System.out.println(findBirdSpecified2.getBirdSpecified());
+//        System.out.println(findBirdSpecified3.getBirdSpecified());
+
+//        Bird findWeight = new Bird();
+//        Bird findWeight2 = new Bird();
+//        Bird findWeight3 = new Bird();
+
+//        findWeight.findWeightFor(3.6);
+//        findWeight2.findWeightFor(4);
+//        findWeight3.findWeightFor(3.6);
+
+//
+//        System.out.println(findWeight.getBirdWeight());
+
+
+//        System.out.println(findWeight.findWeightFor(3.6));
+//        System.out.println(findWeight2.findWeightFor(1));
+//        System.out.println(findWeight3.findWeightFor(11));
+
+
+
+
