f7ef312c34cbec02c584806e9124618dcaa668a6
RitaSkalkovich <skalkovichrita140295@gmail.com>
2021-08-16 18:57:53 +0200

    Cteate class and constructer

---
+++ partOfJava/src/lesson5/task1/Bird.java
@@ -0,0 +1,16 @@
+package lesson5.task1;
+
+public class Bird {
+    String kind;
+    double weight;
+
+    public Bird(String kind, double weight) {
+        this.kind = kind;
+        this.weight = weight;
+
+    }
+
+
+}
+
+
