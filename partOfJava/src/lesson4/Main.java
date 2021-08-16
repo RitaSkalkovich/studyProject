8357682aec56fd9d51cad540aae7be91c40d4a90
RitaSkalkovich <skalkovichrita140295@gmail.com>
2021-08-16 18:18:27 +0200

    Add new line

--- partOfJava/src/lesson4/Main.java
+++
@@ -1,9 +0,0 @@
-package lesson4;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello java");
-        System.out.println("checked commit");
-
-    }
-}
