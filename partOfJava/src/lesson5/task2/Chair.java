92bf8511f8e79233314b3e37d7eabe460adf178d
RitaSkalkovich <skalkovichrita140295@gmail.com>
2021-08-16 19:14:17 +0200

    create inner class legs and constructer

---
+++ partOfJava/src/lesson5/task2/Chair.java
@@ -0,0 +1,26 @@
+package lesson5.task2;
+
+public class Chair {
+    private String producer;
+    private int price;
+
+
+    public Chair(String model, int price) {
+
+        this.producer = producer;
+        this.price = price;
+
+    }
+    public class legs {
+        public void wheel() {
+            System.out.println("На колесах");
+        }
+
+        public void withoutWheel() {
+            System.out.println("Без колесиков");
+        }
+    }
+
+
+
+}
