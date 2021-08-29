e57bc394bc2d8da8e409a68497464809e793aba1
RitaSkalkovich <skalkovichrita140295@gmail.com>
2021-08-29 17:59:40 +0200

    fix mistake and new code

---
+++ partOfJava/src/InOutSystemProject/Record/IdCard.java
@@ -0,0 +1,42 @@
+package InOutSystemProject.Record;
+
+import java.util.Arrays;
+import java.util.Random;
+
+
+public class IdCard {
+
+    public static void main(String[] args) {
+
+        int arrayLength = 10;
+        int upperBounder = 10;
+        int[] array = generateIdCard(arrayLength, upperBounder);
+//        System.out.println(Arrays.toString(array));
+
+        System.out.print(array[0]);
+        System.out.print(array[1]);
+        System.out.print(array[2]);
+        System.out.print(array[3]);
+        System.out.print(array[4]);
+        System.out.print(array[5]);
+        System.out.print(array[6]);
+        System.out.print(array[7]);
+        System.out.print(array[8]);
+        System.out.print(array[9]);
+
+    }
+
+    private static int[] generateIdCard(int arrayLength, int upperBounder) {
+        int[] array = new int[arrayLength];
+
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(upperBounder);
+
+        }
+        return array;
+    }
+}
+
+
+
