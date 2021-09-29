330cc74e1e6c60994a1d7f962ab2addd7284593d
RitaSkalkovich <skalkovichrita140295@gmail.com>
2021-09-29 22:14:35 +0200

    LambdaHomeWork

--- partOfJava/src/LambdaHomeWork/Task1.java
+++ partOfJava/src/LambdaHomeWork/Task1.java
@@ -3,14 +3,21 @@ package LambdaHomeWork;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Task1 {
     public static void main(String[] args) {
         ArrayList<String> collectionList = new ArrayList<>();
         Collections.addAll(collectionList, "ser", "collection", "find", "ser", "collectionCollection", "collectionCollectionMore");
-        collectionList.stream()
+
+        List<String> newCollection = collectionList.stream()
                 .filter(s -> !s.contains("ser")).sorted(Comparator.comparing(x -> x.length(), Comparator.reverseOrder()))
-                .forEach(s -> System.out.println(s));
+                .collect(Collectors.toList());
+
+        for (String s : newCollection) {
+            System.out.println(s);
+        }
+
     }
 }
-
